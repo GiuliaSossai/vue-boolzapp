08e3ede568868c48ef674af3f068b84023db174c
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-15 14:49:57 +0100

    ciclo v-for per creare lista chat

---
+++ assets/js/vue.js
@@ -0,0 +1,199 @@
+const app = new Vue  ({
+   el: '#app',
+
+   data:{
+      contacts: [
+         {
+            nome: 'Michele',
+            avatar: '_1',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               }
+            ]
+         },
+
+         {
+            nome: 'Mauro',
+            avatar: '_2',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               }
+            ]
+         },
+
+         {
+            nome: 'Sofia',
+            avatar: '_3',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok?',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'cosa fai?',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'scrivo codice',
+                  status: 'ricevuto'
+               }
+            ]
+         },
+
+         {
+            nome: 'Cecilia',
+            avatar: '_4',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               }
+            ]
+         },
+
+         {
+            nome: 'Anna',
+            avatar: '_5',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               }
+            ]
+         },
+
+         {
+            nome: 'Giacomo',
+            avatar: '_6',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               }
+            ]
+         },
+
+         {
+            nome: 'Pietro',
+            avatar: '_7',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               }
+            ]
+         },
+
+         {
+            nome: 'Sveva',
+            avatar: '_8',
+            visibile: true,
+            messages: [
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               },
+               {
+                  date: '15/11/2021',
+                  text: 'tutto ok',
+                  status: 'inviato'
+               }
+            ]
+         }
+
+      ]
+
+   },
+
+   methods:{
+
+   }
+
+
+});
